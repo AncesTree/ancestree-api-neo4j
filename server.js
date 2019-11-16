0b0d749e9d408658ebf17499afa27f216ca05cf1
Thibaut <Thibaut.allard-serre@etu.umontpellier.fr>
2019-11-16 15:22:46 +0100

    Fix

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 var cors = require('cors')
 const resource = require('@neode/express')
+const axios = require('axios');
 
 /**
  * Load Neode with the variables stored in `.env` and tell neode to
@@ -21,6 +22,26 @@ const neode = require('neode')
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
+app.use(function (req, res, next) {
+  if (!req.headers.authorization) {
+    res.status(403).send('Unauthorized')
+  }
+  let token = req.headers.authorization;
+  if (token === 'null') {
+    res.status(403).send('Unauthorized')
+  }
+  axios.get('https://ancestree-auth.igpolytech.fr/auth/checktoken', {headers: 
+    {Authorization: token }
+  }).then((result) => {
+    console.log(result.status)
+    if(result.status === 200){
+      next()
+    }
+    else{
+      res.status(403).send('Unauthorized')
+    }
+  })
+})
 app.use(require('./routes/api')(neode));
 app.use('/api/users', resource(neode, 'User'))
 
